263557d384ff96e730cd36402d93bfa334261657
Diwizy <411241035@mahasiswa.undira.ac.id>
2025-12-26 15:04:43 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,3 @@
-import kbb
-
-def handler(request):
-    # Panggil fungsi atau proses dari kbb.py
-    # Kalau cuma pengujian, balas simple saja
-    return {
-        "statusCode": 200,
-        "body": "Server berjalan dengan baik!"
-    }
+def get_response():
+    import kbb
+    return kbb.get_response()
